b5d818b7f6fbefade93f030d3fbae75808ed82a7
Stephen Bodnar <courir123@yahoo.fr>
2017-09-15 01:56:20 -0700

    first commit

--- Sources/App/Models/Post.swift
+++ Sources/App/Models/Post.swift
@@ -1,100 +1,68 @@
+//
+//  File.swift
+//  routing
+//
+//  Created by Stephen Bodnar on 15/09/2017.
+//
+//
+
+import Foundation
 import Vapor
 import FluentProvider
-import HTTP
 
 final class Post: Model {
     let storage = Storage()
     
-    // MARK: Properties and database keys
-    
-    /// The content of the post
-    var content: String
-    
-    /// The column names for `id` and `content` in the database
     static let idKey = "id"
-    static let contentKey = "content"
-
-    /// Creates a new Post
-    init(content: String) {
-        self.content = content
+    static let titleKey = "title"
+    static let uniqueIdKey = "uniqueIdKey"
+    
+    var title: String
+    var uniqueId: String
+    
+    init(title: String, uniqueId: String) {
+        self.title = title
+        self.uniqueId = uniqueId
     }
-
-    // MARK: Fluent Serialization
-
-    /// Initializes the Post from the
-    /// database row
+    
     init(row: Row) throws {
-        content = try row.get(Post.contentKey)
+        title = try row.get(Post.titleKey)
+        uniqueId = try row.get(Post.uniqueIdKey)
     }
-
-    // Serializes the Post to the database
+    
     func makeRow() throws -> Row {
         var row = Row()
-        try row.set(Post.contentKey, content)
+        try row.set(Post.titleKey, title)
+        try row.set(Post.uniqueIdKey, uniqueId)
         return row
     }
 }
 
-// MARK: Fluent Preparation
-
 extension Post: Preparation {
-    /// Prepares a table/collection in the database
-    /// for storing Posts
     static func prepare(_ database: Database) throws {
         try database.create(self) { builder in
             builder.id()
-            builder.string(Post.contentKey)
+            builder.string(Post.titleKey)
+            builder.string(Post.uniqueIdKey)
         }
     }
-
-    /// Undoes what was done in `prepare`
     static func revert(_ database: Database) throws {
         try database.delete(self)
     }
 }
 
-// MARK: JSON
-
-// How the model converts from / to JSON.
-// For example when:
-//     - Creating a new Post (POST /posts)
-//     - Fetching a post (GET /posts, GET /posts/:id)
-//
 extension Post: JSONConvertible {
     convenience init(json: JSON) throws {
         try self.init(
-            content: json.get(Post.contentKey)
+            title: json.get(Post.titleKey),
+            uniqueId: json.get(Post.uniqueIdKey)
         )
     }
-    
     func makeJSON() throws -> JSON {
         var json = JSON()
         try json.set(Post.idKey, id)
-        try json.set(Post.contentKey, content)
+        try json.set(Post.titleKey, title)
+        try json.set(Post.uniqueIdKey, uniqueId)
         return json
-    }
-}
-
-// MARK: HTTP
-
-// This allows Post models to be returned
-// directly in route closures
-extension Post: ResponseRepresentable { }
-
-// MARK: Update
-
-// This allows the Post model to be updated
-// dynamically by the request.
-extension Post: Updateable {
-    // Updateable keys are called when `post.update(for: req)` is called.
-    // Add as many updateable keys as you like here.
-    public static var updateableKeys: [UpdateableKey<Post>] {
-        return [
-            // If the request contains a String at key "content"
-            // the setter callback will be called.
-            UpdateableKey(Post.contentKey, String.self) { post, content in
-                post.content = content
-            }
-        ]
     }
 }
--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -1,25 +1,69 @@
 import Vapor
+ import HTTP
 
 extension Droplet {
     func setupRoutes() throws {
-        get("hello") { req in
-            var json = JSON()
-            try json.set("hello", "world")
-            return json
+        // simple get request:
+        get("hello") { request in
+            return "Hello, Vaporists!"
         }
-
-        get("plaintext") { req in
-            return "Hello, world!"
-        }
-
-        // response to requests to /info domain
-        // with a description of the request
-        get("info") { req in
-            return req.description
-        }
-
-        get("description") { req in return req.description }
         
-        try resource("posts", PostController.self)
+        // nested paths
+        get("hi/how/are/you") { request in
+            return "this path is /hi /how /are /you"
+        }
+        
+        // All routes must return either:
+          // response representable, response, or throw
+        
+        // example of returning response:
+        get("foo") { request in
+            return Response(redirect: "https://apple.com")
+        }
+        
+        
+        get("bar") { request in
+            return Response(status: Status.forbidden)
+        }
+        
+        // return response with JSON
+        get("vapor") { request in
+            return try Response(status: Status.accepted, json: JSON(node: ["hello": "world!"]))
+        }
+        
+        // /post/1 /post/2 ?
+        get("post", Int.parameter) { request in
+            let parameter = try request.parameters.next(Int.self)
+            return "You requested route /post/\(parameter)"
+        }
+        
+        get("post", Int.parameter, "comments") { request in
+            let parameter = try request.parameters.next(Int.self)
+            return "You requested route /post/\(parameter)/comments"
+        }
+        
+        get("heart", String.parameter) { request in
+            let parameter = try request.parameters.next(String.self)
+            return "You requested route /heart/\(parameter)"
+        }
+        
+        get("anythingHere", "*") { request in
+            // this route matches:
+               // /anything/1
+               // /anything/1/2/3
+            return "this is a fallback route"
+        }
+        
+        let v1 = grouped("v1")
+        v1.get("users") { request in
+            return "this route is /users/all"
+        }
+        v1.get("comments") { request in
+            return "this route is /users/none"
+        }
+        
+        
+        
+        
     }
 }
